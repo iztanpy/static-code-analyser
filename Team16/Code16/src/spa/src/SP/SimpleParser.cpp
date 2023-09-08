f527930173990e8c98703725b39f533a6469e41f
Lin Hui Xin Tiffany <tiffanylinnn21@gmail.com>
2023-09-08 23:39:21 +0800

    feat: add SimpleParser

--- Team16/Code16/src/spa/src/SP/SimpleParser.cpp
+++ Team16/Code16/src/spa/src/SP/SimpleParser.cpp
@@ -1,12 +1,104 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 #include "SimpleParser.h"
+//#include "TNode.h"
+//#include "Visitor.h"
+//#include "ParseVisitor.h"
+
+//class ;
 
 // constructor
 SimpleParser::SimpleParser() {}
+AssignmentParser::AssignmentParser() {}
+bool isDebug = false;
 
-int SimpleParser::parse() {
+
+
+int AssignmentParser::parse(const std::vector<Token>& tokens, int curr_index) {
+    TNode lhs = TNode(tokens[curr_index].type, tokens[curr_index].name);
+    TNode root = TNode(tokens[curr_index + 1].type, tokens[curr_index + 1].name);
+    root.addChild(lhs);
+    // check if rhs is expression
+    curr_index += 2;
+
+    while(curr_index <= tokens.size()) {
+        Token curr = tokens[curr_index];
+        if (isDebug) { std::cout << "curr token: " << curr.getValue() << std::endl; }
+
+        // check if curr is constant or variable
+        if (curr.type != "constant" && curr.type != "variable") {
+            if (isDebug) { std::cout << "curr token is not a constant or variable" << std::endl; }
+            return -1;
+        }
+
+        Token next = tokens[curr_index + 1];
+
+        // check if next is ;
+        if (next.type == ";") {
+            if (isDebug) { std::cout << "next token is end token" << std::endl; }
+            // add rhs to root
+            root.addChild(TNode(curr.type, curr.getValue()));
+            curr_index += 1;
+            break;
+        }
+
+        // check if next is operator
+        if (next.type != "plus" && next.type != "minus") {
+            if (isDebug) { std::cout << "next token is not operator token" << std::endl; }
+            return -1;
+        }
+
+        if (isDebug) { std::cout << "next token is operator" << std::endl; }
+
+        // add operator as rhs of root
+        TNode rhs = TNode(next.type, next.getValue());
+        lhs = TNode(curr.type, curr.getValue());
+        rhs.addChild(lhs);
+        root.addChild(rhs);
+
+        // update lhs and root
+        root = rhs;
+
+        // update curr_index
+        curr_index += 2;
+    }
+    curr_index += 1;
+    if (isDebug) { std::cout << "next index" << curr_index << std::endl; }
+    return curr_index;
+}
+
+int SimpleParser::parse(std::vector<Token> tokens) {
+
+    // Visitor
+//    ParseVisitor* visitor = new ParseVisitor();
+    AssignmentParser* assignmentParser = new AssignmentParser();
+
+    //build ast
+    int curr_index = 0;
+
+    while(curr_index < tokens.size()) {
+        Token curr_token = tokens[curr_index];
+        if (curr_token.type == "variable") {
+            Token next_token = tokens[curr_index + 1];
+            if (next_token.type == "equal") {
+                int next_index = assignmentParser->parse(tokens, curr_index);
+                if (next_index == -1) {
+                    if (isDebug) { std::cout << "Error: Invalid token in RHS of assignment" << std::endl; }
+                    return -1;
+                } else {
+                    curr_index = next_index;
+                }
+            }
+        }
+        return curr_index;
+    }
+
     return 0;
+}
+
+std::vector<std::string> SimpleParser::getAllVariables() {
+    return std::vector<std::string>();
 }
--- Team16/Code16/src/spa/src/SP/SimpleParser.h
+++ Team16/Code16/src/spa/src/SP/SimpleParser.h
@@ -2,9 +2,55 @@
 #ifndef SPA_SIMPLEPARSER_H
 #define SPA_SIMPLEPARSER_H
 
+#include <vector>
+#include <string>
+#include "../TNode.h"
+#include "ParseVisitor.h"
+
+struct Token {
+    std::string type;
+    int line;
+
+    std::string name;
+    std::string intValue;
+
+    Token() : type(), line(), name(), intValue() {};
+    Token(std::string t) : type(t), line(), name(), intValue() {};
+    Token(std::string t, int l) : type(t), line(l), name(), intValue() {};
+    Token(std::string t, int l, int i) : type(t), line(l), name(), intValue(std::to_string(i)) {};
+    Token(std::string t, int l, std::string n) : type(t), line(l), name(n), intValue() {};
+
+    std::string getValue() {
+        if (type == "variable") {
+            return name;
+        } else if (type == "constant") {
+            return intValue;
+        } else {
+            return type;
+        }
+    }
+};
+
 class SimpleParser {
     public:
         SimpleParser();
-        int parse();
+//        ParseVisitor* visitor;
+        int parse(std::vector<Token> tokens);
+        std::vector<std::string> getAllVariables();
+
+};
+
+class AssignmentParser {
+public:
+    AssignmentParser();
+    int parse(const std::vector<Token>& tokens, int curr_index);
+};
+
+class Expression {
+public:
+    Expression() = default;
+    static bool isExpression() {
+        return true;
+    };
 };
 #endif //SPA_SIMPLEPARSER_H
