fc38b9cae9c23a9c56336d7880f4da1e2efffb2c
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-10 19:15:33 +0800

    Clean up unused classes and minor changes

--- Team16/Code16/src/spa/src/qps/query_evaluator.cpp
+++
@@ -1,6 +0,0 @@
-#include "qps/query_evaluator.h"
-
-std::list<std::string> QueryEvaluator::evaluate(ParsedQuery& query) {
-  // TODO(phuccuongngo99): Implement this
-  return {"dummy", "value"};
-}
--- Team16/Code16/src/spa/src/qps/suchthat_clause.h
+++ Team16/Code16/src/spa/src/qps/suchthat_clause.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <variant>
 
 #include "qps/declaration.h"
 #include "qps/clause.h"
