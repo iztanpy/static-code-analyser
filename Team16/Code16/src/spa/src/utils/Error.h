f3e5a8e48b1e4b2f671ded0cf93c6fbb3abb600c
soloplxya <shainesnake@gmail.com>
2023-09-15 00:15:50 +0800

    TEAM16-29 Add documentation for error class

--- Team16/Code16/src/spa/src/utils/Error.h
+++ Team16/Code16/src/spa/src/utils/Error.h
@@ -4,19 +4,43 @@
 #include <stdexcept>
 #include <string>
 
+// Created by Shaine
+// ai-gen start (gpt3, 2)
+/**
+ * @class Error
+ * @brief A base class for custom exception handling.
+ */
 class Error : public std::exception {
  public:
+     /**
+     * @brief Constructs an Error object with an optional error message.
+     * @param msg The error message (default is "An error occurred").
+     */
     explicit Error(const std::string& msg = "An error occurred") : message(msg) {}
+
+    /**
+     * @brief Returns a C-style string describing the exception.
+     * @return A pointer to the error message.
+     */
     const char* what() const noexcept override {
        return message.c_str();
     }
+
+    /**
+     * @brief Logs the error message to the standard error stream (std::cerr).
+     */
     virtual void log() {
        std::cerr << "Error: " << message << std::endl;
     }
 
+    /**
+     * @brief Logs a custom error message to the standard error stream (std::cerr).
+     * @param customMessage The custom error message to log.
+     */
     virtual void log(const std::string& customMessage) {
         std::cerr << "Error: " << customMessage << std::endl;
     }
+
  protected:
     std::string message;
 };
@@ -25,6 +49,14 @@ class Error : public std::exception {
 
 class InvalidStatementError : public Error {
  public:
+     /**
+     * @brief Constructs an InvalidStatementError object with an optional error message.
+     * @param msg The error message (default is "Invalid statement").
+     */
     explicit InvalidStatementError(const std::string& msg = "Invalid statement") : Error(msg) {}
+    /**
+     * @brief Logs the error message specific to invalid statements.
+     * Overrides the base class log() function.
+     */
     void log() override;
 };
