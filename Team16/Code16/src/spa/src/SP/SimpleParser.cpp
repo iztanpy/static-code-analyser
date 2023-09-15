794d348827682839ac6c2f8e1a940e30317b9293
Mrinal Ganesh <mrinalganesh@gmail.com>
2023-09-15 22:44:52 +0800

    Update `store_assignments` method signature

--- Team16/Code16/src/spa/src/SP/SimpleParser.cpp
+++ Team16/Code16/src/spa/src/SP/SimpleParser.cpp
@@ -151,7 +151,7 @@ int SimpleParser::parse(const std::vector<Token>& tokens, int curr_index) {
 //                "Invalid token. Sorry the parser can only handle assignment statements currently.");
         }
     }
-    writeFacade->storeAssignments(assignmentParser->getAssignmentStatementsHashset());
+    writeFacade->storeAssignments(assignmentParser->getUsesStatementNumberHashmap(), assignmentParser->getUsesStatementNumberVarHashmap());
     writeFacade->storeVariables(assignmentParser->getVariablesHashset());
     writeFacade->storeConstants(assignmentParser->getConstantsHashset());
     writeFacade->storeUsesVar(assignmentParser->getAssignVarHashmap());
