281167e2f6884c7aa0bbcf70af64ea87a6a9c754
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-20 15:30:01 +0800

    TEAM16-47 Minor change to query evaluator exit code

--- Team16/Code16/src/spa/src/qps/qps.cpp
+++ Team16/Code16/src/spa/src/qps/qps.cpp
@@ -7,11 +7,6 @@ std::unordered_set<std::string> QPS::Evaluate(std::string query) {
 
   ParsedQuery parsed_query = QueryParser::ParseTokenizedQuery(query_tokens);
 
-  std::unordered_set<std::string> raw_results = this->query_evaluator.Evaluate(parsed_query);
-
-  std::unordered_set<std::string> results;
-  for (const std::string& result : raw_results) {
-    results.insert(result);
-  }
-  return results;
+  // TODO(phuccuongngo99): Try catch here please
+  return query_evaluator.Evaluate(parsed_query);
 }
