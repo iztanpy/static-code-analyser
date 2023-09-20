aaf8a2f55bb0b8c53581bb2af2afb9be6a395d52
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-20 11:12:55 +0800

    TEAM16-40 Change implementation to add to result unordered_set

--- Team16/Code16/src/spa/src/qps/query_evaluator/result_formatter.h
+++ Team16/Code16/src/spa/src/qps/query_evaluator/result_formatter.h
@@ -9,7 +9,10 @@
 #include "qps/query_evaluator/constraint.h"
 
 namespace ResultFormatter {
-void CombinationWithTable(
-    const std::unordered_map<ColName, std::vector<Cell>>& umap,
+
+// Functions to get back results which is a combination of
+// all non-existing columns with the constraint table value
+std::unordered_set<std::string> CombinationWithTable(
+    const std::unordered_map<ColName, std::vector<Cell>>& non_existing_values,
     const Table& table, const std::vector<ColName>& order);
 }  // namespace ResultFormatter
