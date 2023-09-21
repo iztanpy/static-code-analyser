857870d77efcd2440b7a630024ca17ce9fc8a2aa
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-21 15:10:27 +0800

    Add UsesVisitor

--- Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/uses_s.cpp
+++ Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/uses_s.cpp
@@ -1,31 +1,57 @@
 #include "qps/clauses/suchthat_clauses/uses_s.h"
 
+struct UsesVisitor {
+  static UnaryConstraint handle(int stmt_num, Declaration& declaration, ReadFacade& pkb_reader) {
+    if (declaration.design_entity == DesignEntity::VARIABLE) {
+      std::unordered_set<std::string> result = pkb_reader.getVariablesUsedBy(stmt_num);
+      return UnaryConstraint{declaration.synonym, result};
+    } else if (declaration.design_entity == DesignEntity::CONSTANT) {
+      return UnaryConstraint{declaration.synonym, pkb_reader.getConstantsUsedBy(stmt_num)};
+    } else {
+      throw QpsSemanticError("Not implemented");
+    }
+  }
+
+  static Constraint handle(int stmt_num, Wildcard& wildcard, ReadFacade& read_facade) {
+    throw QpsSemanticError("Not implemented");
+  }
+  static Constraint handle(int stmt_num, std::string& entity_name, ReadFacade& read_facade) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Declaration& declaration1, Declaration& declaration2, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Declaration& declaration, Wildcard& wildcard, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Declaration& declaration, std::string& entity_name, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Wildcard& wildcard, Declaration& declaration, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Wildcard& wildcard1, Wildcard& wildcard2, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+
+  static Constraint handle(Wildcard& wildcard, std::string& entity_name, ReadFacade& pkb_reader) {
+    throw QpsSemanticError("Not implemented");
+  }
+};
+
 UsesS::UsesS(StmtRef lhs, EntRef rhs) {
   this->rhs = std::move(rhs);
   this->lhs = std::move(lhs);
 }
 
-Constraint UsesS::handle(int stmt_num, Declaration& declaration, ReadFacade& pkb_reader) {
-  if (declaration.design_entity == DesignEntity::VARIABLE) {
-    std::unordered_set<std::string> result = pkb_reader.getVariablesUsedBy(stmt_num);
-    return UnaryConstraint{declaration.synonym, result};
-  } else if (declaration.design_entity == DesignEntity::CONSTANT) {
-    return UnaryConstraint{declaration.synonym, pkb_reader.getConstantsUsedBy(stmt_num)};
-  } else {
-    throw QpsSemanticError("Not implemented");
-  }
-}
-
-Constraint UsesS::handle(int stmt_num, Wildcard& wildcard, ReadFacade& read_facade) {
-  throw QpsSemanticError("Not implemented");
-}
-Constraint UsesS::handle(int stmt_num, std::string& entity_name, ReadFacade& read_facade) {
-  throw QpsSemanticError("Not implemented");
-}
-
 Constraint UsesS::Evaluate(ReadFacade& pkb_reader) {
 //  throw QpsSemanticError("Not implemented");
-  return std::visit([this, &pkb_reader](auto&& lhs_arg, auto&& rhs_arg) {
-    return this->handle(lhs_arg, rhs_arg, pkb_reader);
+  return std::visit([&pkb_reader](auto&& lhs_arg, auto&& rhs_arg) {
+    return Constraint{UsesVisitor::handle(lhs_arg, rhs_arg, pkb_reader)};
   }, lhs, rhs);
 }
--- Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/uses_s.h
+++ Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/uses_s.h
@@ -13,27 +13,4 @@ class UsesS : public SuchThatClause {
   EntRef rhs;
 
   Constraint Evaluate(ReadFacade& pkb_reader) override;
-
- private:
-  Constraint handle(Declaration& declaration1, Declaration& declaration2, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(Declaration& declaration, Wildcard& wildcard, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(Declaration& declaration, std::string& entity_name, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(Wildcard& wildcard, Declaration& declaration, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(Wildcard& wildcard1, Wildcard& wildcard2, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(Wildcard& wildcard, std::string& entity_name, ReadFacade& pkb_reader) {
-    throw QpsSemanticError("Not implemented");
-  }
-  Constraint handle(int stmt_num, Declaration& declaration, ReadFacade& pkb_reader);
-  Constraint handle(int stmt_num, Wildcard& wildcard, ReadFacade& pkb_reader);
-  Constraint handle(int stmt_num, std::string& entity_name, ReadFacade& pkb_reader);
 };
