e6bbda2563609ec9ed5af9dac7c6f836e1b33ab4
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-23 00:22:50 +0800

    TEAM16-55 Done integrating Follows

--- Team16/Code16/src/spa/src/qps/clauses/evaluator_util.h
+++ Team16/Code16/src/spa/src/qps/clauses/evaluator_util.h
@@ -6,6 +6,7 @@
 #include "utils/hash_utils.h"
 
 using IntStringPairSet = std::unordered_set<std::pair<int, std::string>, PairHash>;
+using IntPairSet = std::unordered_set<std::pair<int, int>, PairHash>;
 using StringPairSet = std::unordered_set<std::pair<std::string, std::string>, PairHash>;
 
 struct EvaluatorUtil {
@@ -15,6 +16,14 @@ struct EvaluatorUtil {
     for (const auto& entry : original) {
       std::string first_str = std::to_string(entry.first);
       converted.insert({first_str, entry.second});
+    }
+    return converted;
+  }
+
+  static StringPairSet ToStringPairSet(IntPairSet& original) {
+    StringPairSet converted;
+    for (const auto& entry : original) {
+      converted.insert({std::to_string(entry.first), std::to_string(entry.second)});
     }
     return converted;
   }
--- Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/parent_evaluator.h
+++ Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/parent_evaluator.h
@@ -7,21 +7,21 @@
 #include "qps/clauses/suchthat_clauses/suchthat_clause.h"
 
 struct ParentEvaluator {
-  static bool Handle(int lhs, int rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static bool Handle(int lhs, int rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static UnaryConstraint Handle(int lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static UnaryConstraint Handle(int lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static bool Handle(int lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static bool Handle(int lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static UnaryConstraint Handle(Declaration& lhs, int rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static UnaryConstraint Handle(Declaration& lhs, int rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static BinaryConstraint Handle(Declaration& lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static BinaryConstraint Handle(Declaration& lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static UnaryConstraint Handle(Declaration& lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static UnaryConstraint Handle(Declaration& lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static bool Handle(Wildcard& lhs, int rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static bool Handle(Wildcard& lhs, int rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static UnaryConstraint Handle(Wildcard& lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static UnaryConstraint Handle(Wildcard& lhs, Declaration& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 
-  static bool Handle(Wildcard& lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_FollowT);
+  static bool Handle(Wildcard& lhs, Wildcard& rhs, ReadFacade& pkb_reader, bool is_ParentT);
 };
