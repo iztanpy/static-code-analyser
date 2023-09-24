0516c6f993cf9141339e2df1b83d39ad709a481d
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-09-24 18:34:50 +0800

    TEAM16-61 Update Uses/Modifies SemanticError behaviour

--- Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/suchthat_validator.h
+++ Team16/Code16/src/spa/src/qps/clauses/suchthat_clauses/suchthat_validator.h
@@ -8,15 +8,11 @@ struct SuchThatValidator {
   /*!
    * Rules for validating Pattern clauses
    */
+  inline static const std::unordered_set<DesignEntity> kUsesModifiesValidLhs =
+      {DesignEntity::STMT, DesignEntity::ASSIGN, DesignEntity::READ, DesignEntity::PRINT, DesignEntity::IF_STMT,
+       DesignEntity::WHILE_LOOP, DesignEntity::CALL, DesignEntity::PROCEDURE};
+
   inline static const std::unordered_set<DesignEntity> kUsesModifiesValidRhs = {DesignEntity::VARIABLE};
-
-  inline static const std::unordered_set<DesignEntity> kUsesValidLhs =
-      {DesignEntity::STMT, DesignEntity::ASSIGN, DesignEntity::PRINT, DesignEntity::IF_STMT,
-       DesignEntity::WHILE_LOOP, DesignEntity::CALL, DesignEntity::PROCEDURE};
-
-  inline static const std::unordered_set<DesignEntity> kModifiesValidLhs =
-      {DesignEntity::STMT, DesignEntity::ASSIGN, DesignEntity::READ, DesignEntity::IF_STMT,
-       DesignEntity::WHILE_LOOP, DesignEntity::CALL, DesignEntity::PROCEDURE};
 
   inline static const std::unordered_set<DesignEntity> kParentFollowsValidRhsLhs =
       {DesignEntity::STMT, DesignEntity::ASSIGN, DesignEntity::READ, DesignEntity::PRINT, DesignEntity::IF_STMT,
@@ -31,20 +27,18 @@ struct SuchThatValidator {
   static void ValidateParentFollows(StmtRef lhs, StmtRef rhs);
 
   /*!
-   * Validates the UsesS/UsesP clause
-   * @param lhs is the right hand side of the UsesS/UsesP clause
-   * @param rhs is the left hand side of the UsesS/UsesP clause
-   * Will throw QpsSemanticError if the UsesS/UsesP clause is invalid
+   * Validates Uses and Modifies clause
+   * @param lhs is the right hand side of the Uses and Modifies clause
+   * @param rhs is the left hand side of the Uses and Modifies clause
+   * Will throw QpsSemanticError if the Uses and Modifies clause is invalid
    */
-  static void ValidateUses(StmtRef lhs, EntRef rhs);
-  static void ValidateUses(EntRef lhs, EntRef rhs);
+  static void ValidateUsesModifies(StmtRef lhs, EntRef rhs);
 
   /*!
-   * Validates the ModifiesS/ModifiesP clause
-   * @param lhs is the right hand side of the ModifiesS/ModifiesP clause
-   * @param rhs is the left hand side of the ModifiesS/ModifiesP clause
-   * Will throw QpsSemanticError if the ModifiesS/ModifiesP clause is invalid
+   * Validates Uses and Modifies clause
+   * @param lhs is the right hand side of the Uses and Modifies clause
+   * @param rhs is the left hand side of the Uses and Modifies clause
+   * Will throw QpsSemanticError if the Uses and Modifies clause is invalid
    */
-  static void ValidateModifies(StmtRef lhs, EntRef rhs);
-  static void ValidateModifies(EntRef lhs, EntRef rhs);
+  static void ValidateUsesModifies(EntRef lhs, EntRef rhs);
 };
