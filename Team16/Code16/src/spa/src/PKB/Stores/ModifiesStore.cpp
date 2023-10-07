b44cd4d19e02611ddd6b2fea4ae5f5ffbdfe160a
Mrinal Ganesh <mrinalganesh@gmail.com>
2023-10-07 14:05:29 +0800

    Add support for `ModifiesP`

--- Team16/Code16/src/spa/src/PKB/Stores/ModifiesStore.cpp
+++ Team16/Code16/src/spa/src/PKB/Stores/ModifiesStore.cpp
@@ -1,13 +1,18 @@
 #include "ModifiesStore.h"
 #include <unordered_map>
+#include <utility>
+#include <iostream>
 
 typedef std::string variable;
 typedef std::string constant;
 typedef int statementNumber;
+typedef std::string procedure;
 
 ModifiesStore::ModifiesStore() {
     std::unordered_map<statementNumber, std::unordered_set<variable>> ModifiesVariableMap;
     std::unordered_map<variable, std::unordered_set<statementNumber>> ModifiesVariableMapReverse;
+    std::unordered_map<procedure, std::unordered_set<variable>> ModifiesProcedureMap;
+    std::unordered_map<variable, std::unordered_set<procedure>> ModifiesProcedureMapReverse;
 }
 
 void ModifiesStore::storeModifies(std::unordered_map<statementNumber, std::unordered_set<variable>> relations) {
@@ -18,6 +23,34 @@ void ModifiesStore::storeModifies(std::unordered_map<statementNumber, std::unord
         }
     }
 }
+
+void ModifiesStore::storeModifiesProcedures(std::unordered_map<procedure, std::pair<int, int>> procedures, std::unordered_map<procedure, std::unordered_set<procedure>> callTableStar) {
+    for (auto const& x : procedures) {
+		std::unordered_set<variable> variables;
+		for (int i = x.second.first; i <= x.second.second; i++) {
+			if (ModifiesVariableMap.find(i) != ModifiesVariableMap.end()) {
+				for (auto const& y : ModifiesVariableMap[i]) {
+					variables.insert(y);
+				}
+			}
+		}
+		ModifiesProcedureMap[x.first] = variables;
+		for (auto const& y : variables) {
+			ModifiesProcedureMapReverse[y].insert(x.first);
+		}
+	}
+	for (auto const& x : callTableStar) {
+		for (auto const& y : x.second) {
+			if (ModifiesProcedureMap.find(y) != ModifiesProcedureMap.end()) {
+				for (auto const& z : ModifiesProcedureMap[y]) {
+					ModifiesProcedureMap[x.first].insert(z);
+					ModifiesProcedureMapReverse[z].insert(x.first);
+				}
+			}
+		}
+	}
+}
+
 
 bool ModifiesStore::isModifies(statementNumber statement, variable variable) {
     if (ModifiesVariableMap.find(statement) != ModifiesVariableMap.end()) {
@@ -42,4 +75,46 @@ std::unordered_set<statementNumber> ModifiesStore::modifies(ModifiesStore::varia
 
 std::unordered_set<variable> ModifiesStore::modifies(ModifiesStore::statementNumber statement) {
     return ModifiesVariableMap[statement];
+}
+
+bool ModifiesStore::isModifies(procedure procedure) {
+	if (ModifiesProcedureMap.find(procedure) != ModifiesProcedureMap.end()) {
+		return true;
+	}
+	return false;
+}
+
+std::unordered_set<variable> ModifiesStore::modifiesProcedureProc(procedure procedure) {
+	return ModifiesProcedureMap[procedure];
+}
+
+bool ModifiesStore::isModifies(procedure procedure, variable variable) {
+	if (ModifiesProcedureMap.find(procedure) != ModifiesProcedureMap.end()) {
+		if (ModifiesProcedureMap[procedure].find(variable) != ModifiesProcedureMap[procedure].end()) {
+			return true;
+		}
+	}
+	return false;
+}
+
+std::unordered_set<procedure> ModifiesStore::modifiesProcedure() {
+	std::unordered_set<procedure> procedures;
+	for (auto const& x : ModifiesProcedureMap) {
+		procedures.insert(x.first);
+	}
+	return procedures;
+}
+
+std::unordered_set<procedure> ModifiesStore::modifiesProcedure(variable variable) {
+	return ModifiesProcedureMapReverse[variable];
+}
+
+std::unordered_set<std::pair<procedure, variable>, PairHash> ModifiesStore::modifiesProcedurePair() {
+std::unordered_set<std::pair<procedure, variable>, PairHash> pairs;
+	for (auto const& x : ModifiesProcedureMap) {
+		for (auto const& y : x.second) {
+			pairs.insert(std::make_pair(x.first, y));
+		}
+	}
+	return pairs;
 }
