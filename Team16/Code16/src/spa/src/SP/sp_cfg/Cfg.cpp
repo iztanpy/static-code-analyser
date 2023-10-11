5395835b71e016cf9004d5a5d72f334044d41065
soloplxya <shainesnake@gmail.com>
2023-10-11 19:15:12 +0800

    fix: debug

--- Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.cpp
+++ Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.cpp
@@ -1,40 +1,38 @@
 #include "Cfg.h"
 
-CfgNodePtr Cfg::rootCfgNode = std::make_shared<CfgNode>(0);
+std::shared_ptr<CfgNode> Cfg::rootCfgNode = std::make_shared<CfgNode>(0);
 
-CfgNodePtr Cfg::currNode = rootCfgNode;
+std::shared_ptr<CfgNode> Cfg::currNode = rootCfgNode;
 
 void Cfg::handleStatement(int stmtNumber) {
   currNode->addStmtNumber(stmtNumber);
 }
 
 void Cfg::handleIfStatement(int stmtNumber) {
-  CfgNodePtr ifNode = std::make_shared<CfgNode>(stmtNumber);
+  std::shared_ptr<CfgNode> ifNode = std::make_shared<CfgNode>(stmtNumber);
   currNode->addChildren(ifNode);
   ifNode->setParentNode(currNode);
 }
 
 void Cfg::handleElseStatement(int stmtNumber) {
-  CfgNodePtr elseNode = std::make_shared<CfgNode>(stmtNumber);
+  std::shared_ptr<CfgNode>  elseNode = std::make_shared<CfgNode>(stmtNumber);
   currNode->addChildren(elseNode);
 //  elseNode->addParent(cfgNode); keeping if parent for now not sure if pkb needs this
 }
 
 void Cfg::handleWhileStatement(int stmtNumber) {
-  CfgNodePtr whileNode = std::make_shared<CfgNode>(stmtNumber);
+  std::shared_ptr<CfgNode> whileNode = std::make_shared<CfgNode>(stmtNumber);
   currNode->addChildren(whileNode);
-  CfgNodePtr nextNode = std::make_shared<CfgNode>();
+  std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
   whileNode->addChildren(nextNode);
   currNode = nextNode;
 }
 
 void Cfg::handleEndStatement() {
-  CfgNodePtr endNode = std::make_shared<CfgNode>(-1);
+  std::shared_ptr<CfgNode> endNode = std::make_shared<CfgNode>(-1);
   currNode->addChildren(endNode);
 }
 
-CfgNodeMap Cfg::cfgNodeMap = CfgNodeMap();
-
-CfgNodePtr Cfg::getCfgNode() {
+std::shared_ptr<CfgNode> Cfg::getCfgNode() {
   return rootCfgNode;
 }
--- Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.h
+++ Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.h
@@ -4,22 +4,14 @@
 #include <unordered_map>
 
 #include "CfgNode.h"
-
-typedef std::shared_ptr<CfgNode> CfgNodePtr;
-typedef std::unordered_map<std::string, CfgNodePtr> CfgNodeMap;
-
 class Cfg {
 private:
     static std::shared_ptr<CfgNode> rootCfgNode;
     static std::shared_ptr<CfgNode> currNode;
-    static CfgNodeMap cfgNodeMap;
 
 public:
-    Cfg() {
-        currNode = rootCfgNode;
-    };
-
-    virtual ~Cfg() = default;
+    Cfg();
+    ~Cfg(); // Remove the 'virtual' keyword
 
     static std::shared_ptr<CfgNode> getCfgNode();
     static void handleStatement(int stmtNumber);
@@ -28,3 +20,4 @@ public:
     static void handleWhileStatement(int stmtNumber);
     static void handleEndStatement();
 };
+
