64127f94201a497ba3ca933ed942e8d2fedcc4ac
soloplxya <shainesnake@gmail.com>
2023-10-13 14:31:27 +0800

    chore: update documentation for newly added classes

--- Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.h
+++ Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.h
@@ -6,22 +6,90 @@
 #include <unordered_map>
 
 #include "CfgNode.h"
+/**
+ * @class Cfg
+ * @brief A control flow graph (CFG) representation for a program.
+ *
+ * The `Cfg` class provides functionality to build and manage a control flow graph (CFG) for a program.
+ * It includes methods to handle statements and control flow constructs, as well as access to the CFG nodes.
+ */
 class Cfg {
  private:
+  /**
+   * @brief Pointer to the current CFG node being constructed.
+   *
+   * This pointer keeps track of the current node while constructing the CFG.
+   */
     static std::shared_ptr<CfgNode> currNode;
 
  public:
     Cfg() = default;
     virtual ~Cfg() = default;
-
+    /**
+    * @brief Get the current CFG node.
+    *
+    * This method returns a pointer to the current CFG node being constructed.
+    *
+    * @return A shared pointer to the current CFG node.
+    */
     static std::shared_ptr<CfgNode> getCfgNode();
+    /**
+    * @brief Handle a general statement in the program.
+    *
+    * This method is used to handle a general statement in the program and update the CFG accordingly.
+    *
+    * @param stmtNumber The statement number of the current statement.
+    */
     static void handleStatement(int stmtNumber);
+    /**
+    * @brief Handle an "if" statement in the program.
+    *
+    * This method is used to handle an "if" statement in the program and update the CFG accordingly.
+    *
+    * @param stmtNumber The statement number of the "if" statement.
+    */
     static void handleIfStatement(int stmtNumber);
+    /**
+    * @brief Handle an "else" statement in the program.
+    *
+    * This method is used to handle an "else" statement in the program and update the CFG accordingly.
+    *
+    * @param stmtNumber The statement number of the "else" statement.
+    */
     static void handleElseStatement(int stmtNumber);
+    /**
+    * @brief Handle a "while" statement in the program.
+    *
+    * This method is used to handle a "while" statement in the program and update the CFG accordingly.
+    *
+    * @param stmtNumber The statement number of the "while" statement.
+    */
     static void handleWhileStatement(int stmtNumber);
+    /**
+     * @brief Handle an "end" statement.
+     *
+     * This method is used to handle an "end" statement in the program and update the CFG accordingly.
+     */
     static void handleEndStatement();
+    /**
+     * @brief Handle an end of an "else" statement.
+     *
+     * This method is used to handle an end of an "else" statement in the program and update the CFG accordingly.
+     */
     static void handleEndElseStatement();
+    /**
+     * @brief Handle an end of a "while" statement in the program.
+     *
+     * This method is used to handle an end of a "while' statement in the program and update the CFG accordingly.
+     *
+     * @param stmtNumber The statement number of the "while" statement.
+     */
     static void handleEndWhileStatement(int stmtNumber);
+    /**
+     * @brief Handle an end of an "if" statement in the program.
+     *
+     * This method is used to handle an end of an "if" statement in the program and update the CFG accordingly.
+    */
     static void handleEndIfStatement();
     static std::shared_ptr<CfgNode> rootCfgNode;
     static std::stack<std::shared_ptr<CfgNode>> elseEndNodeStack;
--- Team16/Code16/src/spa/src/SP/sp_cfg/CfgNode.h
+++ Team16/Code16/src/spa/src/SP/sp_cfg/CfgNode.h
@@ -4,8 +4,18 @@
 #include <set>
 #include <unordered_set>
 
+/**
+ * @class CfgNode
+ * @brief A node in a control flow graph (CFG).
+ *
+ * The `CfgNode` class represents a node in a control flow graph (CFG) for a program. Each node can have child nodes,
+ * a parent node, and a set of associated statement numbers.
+ */
 class CfgNode {
  private:
+  /**
+   * @brief Set of child nodes connected to this node in the CFG.
+  */
   std::unordered_set<std::shared_ptr<CfgNode>> children;
   std::shared_ptr<CfgNode> parentNode = nullptr;
   std::set<int> statementNumberSet;
@@ -19,18 +29,41 @@ class CfgNode {
 
   virtual ~CfgNode() = default;
 
+  /**
+   * @brief Add a child node to this node in the CFG.
+   *
+   * @param child A shared pointer to the child node to be added.
+   */
   void addChildren(const std::shared_ptr<CfgNode>& child) {
     children.insert(child);
   }
 
+  /**
+   * @brief Add a statement number to the set of statement numbers associated with this node.
+   *
+   * @param stmtNumber The statement number to be added.
+  */
   void addStmtNumber(int stmtNumber) {
     statementNumberSet.insert(stmtNumber);
   }
 
+  /**
+   * @brief Set the parent node of this node in the CFG.
+   *
+   * @param parent A shared pointer to the parent node.
+  */
   void setParentNode(const std::shared_ptr<CfgNode>& parent) {
     parentNode = parent;
   }
 
+  /**
+  * @brief Get the parent node associated with a specific statement number.
+  *
+  * This method traverses the parent nodes to find the parent associated with a specific statement number.
+  *
+  * @param stmtNumber The statement number for which to find the parent node.
+  * @return A shared pointer to the parent node, or nullptr if not found.
+  */
   std::shared_ptr<CfgNode> getParentNode(int stmtNumber) {
     std::shared_ptr<CfgNode> parent = parentNode;
     while (parent != nullptr) {
@@ -43,6 +76,13 @@ class CfgNode {
     return parentNode;
   }
 
+  /**
+  * @brief Check if this node is an end node in the CFG.
+  *
+  * An end node is typically associated with a statement number of -1.
+  *
+  * @return `true` if this node is an end node, `false` otherwise.
+  */
   bool isEndNode() {
     if (statementNumberSet.size() == 1) {
       return *statementNumberSet.begin() == -1;
