d92d7e624d810d9d08d355e66e815622ccda185f
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-10-16 21:25:00 +0800

    Team16-95 Add documentations

--- Team16/Code16/src/spa/src/qps/query_parser/parsed_query.h
+++ Team16/Code16/src/spa/src/qps/query_parser/parsed_query.h
@@ -9,6 +9,11 @@
 #include "qps/clauses/pattern_clauses/pattern_clause.h"
 #include "utils/clauses_types.h"
 
+/**
+ * A struct that represents a parsed query.
+ * selects: a vector of synonyms that are selected in the query, vector of size 0 represents select BOOLEAN
+ * clauses: a vector of clauses that are in the query
+ */
 struct ParsedQuery {
   std::vector<synonym> selects;
   std::vector<std::unique_ptr<Clause>> clauses;
