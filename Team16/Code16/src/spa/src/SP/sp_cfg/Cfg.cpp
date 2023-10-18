56b0567d2e1c77639888196a216087ccfc789803
Lin Hui Xin Tiffany <tiffanylinnn21@gmail.com>
2023-10-19 00:05:44 +0800

    TEAM16-84 Fix: if/else/while edge cases

--- Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.cpp
+++ Team16/Code16/src/spa/src/SP/sp_cfg/Cfg.cpp
@@ -11,120 +11,125 @@ std::stack<std::set<int>> Cfg::nextParentStack = std::stack<std::set<int>>();
 std::unordered_map<int, std::set<int>> Cfg::nextStatementNumberHashmap = std::unordered_map<int, std::set<int>>();
 
 
-
 void Cfg::handleStatement(int stmtNumber) {
-    if (currNode->getStmtNumberSet().empty() && !nextParentStack.empty()) {
-        retrieveParent(stmtNumber);
-    }
-    if (!currNode->getStmtNumberSet().empty()) {
-        nextStatementNumberHashmap[currNode->getLastStatementNumber()].insert(stmtNumber);
-    }
+    storeNextRelationship(currNode->getLastStatementNumber(), stmtNumber, false);
     currNode->addStmtNumber(stmtNumber);
 }
 
 void Cfg::handleIfStatement(int stmtNumber) {
-  if (currNode->getStmtNumberSet().empty()) {
-    if (!nextParentStack.empty()) {
-        retrieveParent(stmtNumber);
-    }
-    currNode->addStmtNumber(stmtNumber);
-  } else {
-    nextStatementNumberHashmap[currNode->getLastStatementNumber()].insert(stmtNumber);
-    std::shared_ptr<CfgNode> ifNode = std::make_shared<CfgNode>(stmtNumber);
-    currNode->addChildren(ifNode);
-    currNode = ifNode;
-  }
-  keyNodesStack.push(currNode);
-  nextParentStack.push({ stmtNumber });
-  std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
-  currNode->addChildren(nextNode);
-  currNode = nextNode;
+    storeNextRelationship(currNode->getLastStatementNumber(), stmtNumber);
+    addStmtNumberToEmptyOrNewCfgNode(stmtNumber);
+    createNewEmptyCfgNode();
 }
 
 void Cfg::handleElseStatement() {
-  std::shared_ptr<CfgNode> elseNode = std::make_shared<CfgNode>();
-  std::shared_ptr<CfgNode> parent = keyNodesStack.top();
-  keyNodesStack.pop();
-  nextParentStack.push({ parent->getLastStatementNumber() });
-  parent->addChildren(elseNode);
-  keyNodesStack.push(currNode);
-  currNode = elseNode;
+    std::shared_ptr<CfgNode> parent = retrieveTopKeyNode();
+    nextParentStack.push({ parent->getLastStatementNumber() });
+    keyNodesStack.push(currNode);
+    currNode = parent;
+    createNewEmptyCfgNode();
 }
 
 void Cfg::handleWhileStatement(int stmtNumber) {
-  if (currNode->getStmtNumberSet().empty()) {
-    if (!nextParentStack.empty()) {
-        retrieveParent(stmtNumber);
-    }
-    currNode->addStmtNumber(stmtNumber);
-  } else {
-    nextStatementNumberHashmap[currNode->getLastStatementNumber()].insert(stmtNumber);
-    std::shared_ptr<CfgNode> whileNode = std::make_shared<CfgNode>(stmtNumber);
-    currNode->addChildren(whileNode);
-    currNode = whileNode;
-  }
-  keyNodesStack.push(currNode);
-  nextParentStack.push({ stmtNumber });
-  std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
-  currNode->addChildren(nextNode);
-  currNode = nextNode;
+    storeNextRelationship(currNode->getLastStatementNumber(), stmtNumber);
+    addStmtNumberToEmptyOrNewCfgNode(stmtNumber);
+    createNewEmptyCfgNode();
 }
 
 void Cfg::handleEndProcedureStatement() {
-  rootCfgNode = std::make_shared<CfgNode>();
-  currNode = rootCfgNode;
+    rootCfgNode = std::make_shared<CfgNode>();
+    currNode = rootCfgNode;
 }
 
 void Cfg::handleEndElseStatement() {
-    std::shared_ptr<CfgNode> endNode = keyNodesStack.top();
-    keyNodesStack.pop();
+    std::shared_ptr<CfgNode> endNode = retrieveTopKeyNode();
     currNode->addChildren(endNode);
-    std::set<int> previousParent = nextParentStack.top();
-    previousParent.insert(currNode->getLastStatementNumber());
-    nextParentStack.pop();
-    nextParentStack.push(previousParent);
+    pushLastIfElseNumbersOntoNextStack();
     currNode = endNode;
 }
 
 void Cfg::handleEndWhileStatement() {
-  // need to handle the case where curr node is empty (i.e. previous node is an end node from if else)
-  std::shared_ptr<CfgNode> parentNode = keyNodesStack.top();
-  keyNodesStack.pop();
-  currNode->addChildren(parentNode);
-  if (currNode->getStmtNumberSet().empty()) {
-      if (!nextParentStack.empty()) {
-          retrieveParent(parentNode->getLastStatementNumber());
-          nextParentStack.push({ parentNode->getLastStatementNumber() });
-      }
-  } else {
-      nextStatementNumberHashmap[currNode->getLastStatementNumber()].insert(parentNode->getLastStatementNumber());
-      nextParentStack.push({ parentNode->getLastStatementNumber() });
-  }
-  std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
-  parentNode->addChildren(nextNode);
-  currNode = nextNode;
+    std::shared_ptr<CfgNode> parentNode = retrieveTopKeyNode();
+    currNode->addChildren(parentNode);
+    storeNextRelationship(currNode->getLastStatementNumber(), parentNode->getLastStatementNumber());
+    currNode = parentNode;
+    createNewEmptyCfgNode();
 }
 
 void Cfg::handleEndIfStatement(bool hasElse) {
-    nextParentStack.push({ currNode->getLastStatementNumber() });
-    std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
-    currNode->addChildren(nextNode);
-    currNode = nextNode;
-    if (hasElse == false) {
-      keyNodesStack.pop();
+    if (!currNode->getStmtNumberSet().empty()) {
+        int lastIfNumber = currNode->getLastStatementNumber();
+        nextParentStack.push({ lastIfNumber });
+    }
+    createNewEmptyCfgNode();
+    if (!hasElse) {
+        keyNodesStack.pop();
     }
 }
 
 std::shared_ptr<CfgNode> Cfg::getCfgNode() {
-  return rootCfgNode;
+    return rootCfgNode;
 }
 
-void Cfg::retrieveParent(int stmtNumber) {
-    std::set parents = nextParentStack.top();
-    std::set<int>::iterator it;
-    for (it = parents.begin(); it != parents.end(); ++it) {
-        int parent = *it;
-        nextStatementNumberHashmap[parent].insert(stmtNumber);
+std::shared_ptr<CfgNode> Cfg::retrieveTopKeyNode() {
+    std::shared_ptr<CfgNode> topKeyNode = keyNodesStack.top();
+    keyNodesStack.pop();
+    return topKeyNode;
+}
+
+void Cfg::createNewEmptyCfgNode() {
+    std::shared_ptr<CfgNode> nextNode = std::make_shared<CfgNode>();
+    currNode->addChildren(nextNode);
+    currNode = nextNode;
+}
+
+void Cfg::addStmtNumberToEmptyOrNewCfgNode(int stmtNumber) {
+    if (currNode->getStmtNumberSet().empty()) {
+        currNode->addStmtNumber(stmtNumber);
+    } else {
+        std::shared_ptr<CfgNode> newNode = std::make_shared<CfgNode>(stmtNumber);
+        currNode->addChildren(newNode);
+        currNode = newNode;
     }
-    nextParentStack.pop();
+    keyNodesStack.push(currNode);
+}
+
+void Cfg::retrieveParentIfNotEmpty(int stmtNumber) {
+    if (!nextParentStack.empty()) {
+        std::set parents = nextParentStack.top();
+        std::set<int>::iterator it;
+        for (it = parents.begin(); it != parents.end(); ++it) {
+            int parent = *it;
+            nextStatementNumberHashmap[parent].insert(stmtNumber);
+        }
+        nextParentStack.pop();
+    }
+}
+
+void Cfg::pushLastIfElseNumbersOntoNextStack() {
+    if (currNode->getStmtNumberSet().empty()) {
+        std::set<int> lastNumbersInElse = nextParentStack.top();
+        nextParentStack.pop();
+        std::set<int> lastNumbersInIf = nextParentStack.top();
+        nextParentStack.pop();
+        lastNumbersInIf.insert(lastNumbersInElse.begin(), lastNumbersInElse.end());
+        nextParentStack.push(lastNumbersInIf);
+    } else {
+        int lastNumberInElse = currNode->getLastStatementNumber();
+        std::set<int> lastNumbersInIf = nextParentStack.top();
+        nextParentStack.pop();
+        lastNumbersInIf.insert(lastNumberInElse);
+        nextParentStack.push(lastNumbersInIf);
+    }
+}
+
+void Cfg::storeNextRelationship(int lastStmtNumber, int nextStmtNumber, bool shouldPushToNextStack) {
+    if (lastStmtNumber == -2) {
+        retrieveParentIfNotEmpty(nextStmtNumber);
+    } else {
+        nextStatementNumberHashmap[lastStmtNumber].insert(nextStmtNumber);
+    }
+    if (shouldPushToNextStack) {
+        nextParentStack.push({ nextStmtNumber });
+    }
 }
