8854965d3e792b0708cd43323516fdc2e7ce9db7
Cuong Ngo <41280374+phuccuongngo99@users.noreply.github.com>
2023-10-20 01:46:19 +0800

    Team16-95 Incomplete unit tests

---
+++ Team16/Code16/src/unit_testing/src/TestClauseSet.cpp
@@ -0,0 +1,80 @@
+#include <catch.hpp>
+
+#include "qps/qps.h"
+#include "qps/clauses/clause.h"
+#include "qps/clauses/suchthat_clauses/suchthat_clauses_all.h"
+#include "qps/clauses/pattern_clauses/pattern_clause.h"
+
+// TODO(phuccuongngo99): This file is to test putting clauses together in a ClauseSet
+// and also QueryParser from a lot of clauses
+
+TEST_CASE("Test 1") {
+  std::string
+      sample_query_1 = "procedure p; Select p such that Calls(p, _) such that Calls(_, p) such that Parent(_, _)";
+  ParsedQuery parsed_query = QueryParser::ParseTokenizedQuery(sample_query_1);
+  REQUIRE(parsed_query.clauses.size() == 4);
+}
+
+TEST_CASE("Test 2") {
+  auto s1 = std::make_unique<SelectClause>();
+  s1->declaration = {"p", DesignEntity::PROCEDURE};
+
+  auto s2 = std::make_unique<SelectClause>();
+  s2->declaration = {"v", DesignEntity::PROCEDURE};
+
+  ClauseSet clause_set;
+  clause_set.insert(std::move(s1));
+  clause_set.insert(std::move(s2));
+
+  REQUIRE(clause_set.size() == 2);
+}
+
+TEST_CASE("Test 3") {
+  auto s1 = std::make_unique<Calls>(Declaration{"p", DesignEntity::PROCEDURE}, Wildcard::Value);
+  auto s2 = std::make_unique<Calls>(Declaration{"x", DesignEntity::PROCEDURE}, Wildcard::Value);
+//  auto s2 = std::make_unique<Parent>(Wildcard::Value, Declaration{"p", DesignEntity::STMT});
+
+  size_t hash1 = s1->Hash();
+  size_t hash2 = s2->Hash();
+
+  ClauseSet clause_set;
+  clause_set.insert(std::move(s1));
+  clause_set.insert(std::move(s2));
+
+  REQUIRE(clause_set.size() == 2);
+}
+
+TEST_CASE("Test 4") {
+  auto s1 = std::make_unique<WhilePattern>(Declaration{"p", DesignEntity::WHILE_LOOP}, Wildcard::Value);
+  auto s2 = std::make_unique<WhilePattern>(Declaration{"p", DesignEntity::WHILE_LOOP}, Wildcard::Value);
+
+//  size_t hash1 = s1->Hash();
+//  size_t hash2 = s2->Hash();
+//
+//  REQUIRE(s1->equals(s2.get()));
+
+  ClauseSet clause_set;
+  clause_set.insert(std::move(s1));
+  clause_set.insert(std::move(s2));
+
+  REQUIRE(clause_set.size() == 2);
+}
+
+TEST_CASE("Test 5") {
+  auto s1 = std::make_unique<AssignPattern>(
+      Declaration{"x", DesignEntity::ASSIGN}, Wildcard::Value, ExactExpr{"x"});
+
+  auto s2 = std::make_unique<AssignPattern>(
+      Declaration{"x", DesignEntity::ASSIGN}, Wildcard::Value, ExactExpr{"y"});
+
+//  size_t hash1 = s1->Hash();
+//  size_t hash2 = s2->Hash();
+//
+//  REQUIRE(s1->equals(s2.get()));
+
+  ClauseSet clause_set;
+  clause_set.insert(std::move(s1));
+  clause_set.insert(std::move(s2));
+
+  REQUIRE(clause_set.size() == 2);
+}
