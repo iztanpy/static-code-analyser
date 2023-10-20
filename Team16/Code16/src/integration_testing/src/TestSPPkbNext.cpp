abbdf1f4917643dc3b7f5bc67cefd57efc3e0399
iztanpy <61683649+iztanpy@users.noreply.github.com>
2023-10-20 17:01:19 +0800

    unit test Next star

--- Team16/Code16/src/integration_testing/src/TestSPPkbNext.cpp
+++ Team16/Code16/src/integration_testing/src/TestSPPkbNext.cpp
@@ -77,3 +77,38 @@ TEST_CASE("Test Next Star") {
     REQUIRE(readFacade.NextStar(StmtEntity::kIf, StmtEntity::kStmt) ==std::set<std::pair<statementNumber, statementNumber>>(
             {{1,2}, {1,3}, {1,4}, {1,5}, {1,6}, {1,7}, {1,8}, {1,9}, {4,5}, {4,6}, {4,7}, {4,8},{4,9}}));
 }
+
+
+TEST_CASE("Test Next Star 2") {
+    auto nextStore = NextStore();
+    std::unique_ptr<PKB> pkb_ptr = std::make_unique<PKB>();
+    auto writeFacade = WriteFacade(*pkb_ptr);
+    auto sourceProcessor = SourceProcessor(&writeFacade);
+    auto readFacade = ReadFacade(*pkb_ptr);
+    std::string simpleProgram4 = R"(procedure Second {
+    while (x==0) {
+        if (x==1) then {
+            while (x==0) {
+                help = help +1;
+            }
+        } else {
+            if (x == 2) then {
+                 help = help +1;
+                 help = help +2;
+                 help = help +3;
+            }
+            test = test + 1;
+        }
+        a = a + b;
+      }
+      })";
+    sourceProcessor.processSource(simpleProgram4);
+    std::unordered_map<int, std::shared_ptr<CfgNode> > cfgLegend = sourceProcessor.getStmtNumberToCfgNodeHashmap();
+    auto map = sourceProcessor.getNextStatementMap();
+    writeFacade.storeCfgLegend(cfgLegend);
+    writeFacade.storeNext(map);
+    REQUIRE(readFacade.NextStar(StmtEntity::kIf, StmtEntity::kStmt) ==std::set<std::pair<statementNumber, statementNumber>>(
+            {{2,1}, {2,2}, {2,3}, {2,4}, {2,5}, {2,6}, {2,7}, {2,8}, {2,9}, {2,10},
+             {5,1}, {5,2}, {5,3}, {5,4}, {5,5}, {5,6}, {5,7}, {5,8}, {5,9}, {5,10}}));
+}
+
